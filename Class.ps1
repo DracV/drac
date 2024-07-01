7159b910b18db481e975ad4cc0e57c1b3d676768
DracV <22818542+DracV@users.noreply.github.com>
2024-07-01 02:40:32 -0700

    Create Class.ps1
    powershell script - winget install multi apps

---
+++ Class.ps1
@@ -0,0 +1,7 @@
+winget install 7zip.7zip;
+winget install Google.Chrome;
+winget install Oracle.JavaRuntimeEnvironment;
+winget install XPDNZJFNCR1B07;
+winget install Zoom.Zoom;
+winget install XPDP273C0XHQH2;
+winget install VideoLAN.VLC
